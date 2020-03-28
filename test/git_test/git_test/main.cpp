5d73912841bcefc2a5c24d84f68a2e377361697b
Andrzej Jurga <andrzej.jurga@uczen.zsk.poznan.pl>
2020-03-28 17:01:59 +0100

    Test andrzej laptop

---
+++ test/git_test/git_test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int temp = 1;
+	cout << "Gratuluje udanej konfiguracji. Teraz zwieksz zmianna temp w kodzie o 1 " << temp;
+
+	return 2;
+}
