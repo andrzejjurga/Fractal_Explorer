d46e7b3b0569e407ee7ec0e4eea7a488d1150f19
sebastianjerzykiewicz <s.jerzykiewicz@gmail.com>
2020-05-08 11:22:39 +0200

    Folder Resources

--- DEFINITIONS.hpp
+++ DEFINITIONS.hpp
@@ -1,10 +1,10 @@
 #define MAX_NUMBER_OF_ITEMS 3
 
-#define FONT_FILE_PATH "Resources/Fonts/font.ttf"
-#define BACKGROUND_FILE_PATH "Resources/Images/bg.jpg"
-
 #define SCREEN_WIDTH 626
 #define SCREEN_HEIGHT 626
+
+#define FONT_FILE_PATH "Resources/Fonts/font.ttf"
+#define BACKGROUND_FILE_PATH "Resources/Images/bg.jpg"
 
 
 
